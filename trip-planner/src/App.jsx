fc5d21ecd52b9f3461143763bc409049c2569c35
Truc Duong <trucdg@bu.edu>
2023-04-06 22:16:38 -0400

    make Layout the element to the main route and other pages as children of Layout

--- trip-planner/src/App.jsx
+++ trip-planner/src/App.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainNavigation from "./components/MainNavigation";
+import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Weather from "./pages/Weather";
 import "./App.css";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/weather", element: <Weather /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/weather", element: <Weather /> },
+    ],
+  },
 ]);
 
 function App() {
