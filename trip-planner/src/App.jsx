3d9620292688eac5548f5a4e1d08659a6d0044eb
Truc Duong <trucdg@bu.edu>
2023-04-06 22:33:22 -0400

    create an ErrorPage and add it to the page route definition

--- trip-planner/src/App.jsx
+++ trip-planner/src/App.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Weather from "./pages/Weather";
+import ErrorPage from "./pages/ErrorPage";
 import "./App.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Home /> },
       { path: "/weather", element: <Weather /> },
---
+++ trip-planner/src/pages/ErrorPage.jsx
@@ -0,0 +1,5 @@
+const ErrorPage = () => {
+  return <h1> OOpss! 404</h1>;
+};
+
+export default ErrorPage;
