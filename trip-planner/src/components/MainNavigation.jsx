a7d0111536ca089dfdb7fab5673297ed8289d497
Truc Duong <trucdg@bu.edu>
2023-04-06 21:52:04 -0400

    use bootstrap class to style MainNavigation component

--- trip-planner/src/components/MainNavigation.jsx
+++ trip-planner/src/components/MainNavigation.jsx
@@ -1,7 +1,7 @@
 function MainNavigation() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
             TripPlanner
@@ -20,7 +20,7 @@ function MainNavigation() {
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+                <a class="nav-link" href="/">
                   Home
                 </a>
               </li>
