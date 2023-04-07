6a3ec9e71b74f7be89a0215a5e703dc97200ce8f
Truc Duong <trucdg@bu.edu>
2023-04-07 00:12:52 -0400

    add initial styling to the Weather page

--- trip-planner/src/pages/Weather.jsx
+++ trip-planner/src/pages/Weather.jsx
@@ -1,7 +1,11 @@
+import classes from "./Weather.module.css";
+
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather Forecast</h1>
+    <div className={classes["weather-content-box"]}>
+      <div className={classes["weather-content"]}>
+        <h1>Weather Forecast</h1>
+      </div>
     </div>
   );
 };
