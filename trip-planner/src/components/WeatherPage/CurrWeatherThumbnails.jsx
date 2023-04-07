96484a843cd9d083fadce8c94b0d85bd2b85a3c6
Truc Duong <trucdg@bu.edu>
2023-04-07 01:23:38 -0400

    use bootstrap to fix stylings for the CurrWeatherThumbnails component

--- trip-planner/src/components/WeatherPage/CurrWeatherThumbnails.jsx
+++ trip-planner/src/components/WeatherPage/CurrWeatherThumbnails.jsx
@@ -1,3 +1,5 @@
+import "./CurrWeatherThumbnails.css";
+
 const CurrWeatherThumbNails = ({
   cityName,
   temp,
@@ -5,8 +7,14 @@ const CurrWeatherThumbNails = ({
   weatherDescription,
 }) => {
   return (
-    <div className="d-flex">
-      <ul>
+    <div className="weather-thumbnail-cont">
+      <p>
+        TripPlanner uses data from the
+        <a href="https://openweathermap.org/" target="_blank">
+          OpenWeather API
+        </a>
+      </p>
+      <ul className="d-flex flex-column flex-lg-row justify-content-center align-items-center">
         <li className="card">
           <h4>{cityName}</h4>
         </li>
