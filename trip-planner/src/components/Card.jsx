84c635658161303cf26fd1b526d1d5c8a2ecf1ad
Truc Duong <trucdg@bu.edu>
2023-04-16 01:39:39 -0400

    add more stylings to the Card component

--- trip-planner/src/components/Card.jsx
+++ trip-planner/src/components/Card.jsx
@@ -5,12 +5,12 @@ import classes from "./Card.module.css";
 const Card = (props) => {
   return (
     <div className={classes["post-card"]}>
-      <Link to="">
+      <Link to="" className={classes.edit}>
         <i className="fa-solid fa-ellipsis-vertical"></i>
       </Link>
-      <h2>{props.title}</h2>
-      <h3>{"by " + props.author}</h3>
-      <p>{props.description}</p>
+      <h2 className={classes.title}>{props.title}</h2>
+      <h4 className={classes.author}>{"by " + props.author}</h4>
+      <p className={classes.description}>{props.description}</p>
     </div>
   );
 };
