3afae572c72af9231e5ad1bd090e670620a97cc0
Truc Duong <trucdg@bu.edu>
2023-04-16 01:53:28 -0400

    add like button and useState hook to keep track of the number of likes

--- trip-planner/src/components/Card.jsx
+++ trip-planner/src/components/Card.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./Card.module.css";
 
 const Card = (props) => {
+  const [likesCount, setLikesCount] = useState(0);
+  const likeClickHandler = () => {
+    setLikesCount((prevState) => prevState + 1);
+  };
   return (
     <div className={classes["post-card"]}>
       <Link to="" className={classes.edit}>
@@ -11,6 +16,9 @@ const Card = (props) => {
       <h2 className={classes.title}>{props.title}</h2>
       <h4 className={classes.author}>{"by " + props.author}</h4>
       <p className={classes.description}>{props.description}</p>
+      <button className={classes.likeBtn} onClick={likeClickHandler}>
+        <i className="fa-solid fa-thumbs-up"></i> Likes: {likesCount}
+      </button>
     </div>
   );
 };
