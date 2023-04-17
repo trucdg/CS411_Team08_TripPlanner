ff6960aa0a0a830ae0efdc34d040d747c24a3c72
Truc Duong <trucdg@bu.edu>
2023-04-16 23:18:09 -0400

    fix typo in client.js

--- trip-planner/src/client.js
+++ trip-planner/src/client.js
@@ -4,4 +4,4 @@ import { createClient } from "@supabase/supabase-js";
 const URL = "https://noltkejfjldnlhfnwnsc.supabase.co";
 const API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vbHRrZWpmamxkbmxoZm53bnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE2MDU4MjEsImV4cCI6MTk5NzE4MTgyMX0.WZ5NZtaKcWeJx0OZdwMrB1u9s1xDVPWUDel-__ApDJM";
-export const supabase = createClient(URL, APIKEY);
+export const supabase = createClient(URL, API_KEY);
