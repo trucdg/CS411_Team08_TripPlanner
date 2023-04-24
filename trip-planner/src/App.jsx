24462dcdc3d32c1293f449627f9c9391a1f4c727
Truc Duong <trucdg@bu.edu>
2023-04-24 01:05:30 -0400

    add routes to the Signup and Login page

--- trip-planner/src/App.jsx
+++ trip-planner/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   createBrowserRouter,
   Route,
@@ -9,23 +10,43 @@ import Home from "./pages/Home/Home";
 import Weather from "./pages/Weather/Weather";
 import TravelFeed from "./pages/TravelFeed/TravelFeed";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import SignUpForm from "./pages/Auth/SignUp";
+import LoginForm from "./pages/Auth/Login";
+import WelcomePage from "./pages/Welcome/WelcomePage";
 
 import "./App.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/weather", element: <Weather /> },
-      { path: "/feed/*", element: <TravelFeed /> },
-    ],
-  },
-]);
-
 function App() {
+  const [token, setToken] = useState(false);
+
+  if (token) {
+    sessionStorage.setItem("token", JSON.stringify(token));
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
+      setToken(data);
+      console.log(data);
+    }
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/weather", element: <Weather /> },
+        { path: "/feed/*", element: <TravelFeed /> },
+        { path: "/signup", element: <SignUpForm /> },
+        { path: "/login", element: <LoginForm setToken={setToken} /> },
+        { path: "/welcome", element: <WelcomePage token={token} /> },
+      ],
+    },
+  ]);
+
   return <RouterProvider router={router} />;
 }
 
