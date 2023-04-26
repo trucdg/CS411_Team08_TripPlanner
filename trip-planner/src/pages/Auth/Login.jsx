b22ae6187b4644121820078f02cb23c05308f019
Truc Duong <trucdg@bu.edu>
2023-04-25 22:05:53 -0400

    add stylings to the Login page

--- trip-planner/src/pages/Auth/Login.jsx
+++ trip-planner/src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { supabase } from "../../client";
+import classes from "./Login.module.css";
 
 const LoginForm = ({ setToken }) => {
   let navigate = useNavigate();
@@ -40,7 +41,7 @@ const LoginForm = ({ setToken }) => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <input placeholder="Email" name="email" onChange={handleChange} />
         <input
           placeholder="Password"
@@ -52,7 +53,59 @@ const LoginForm = ({ setToken }) => {
       </form>
       <p>
         Don't have an account? <Link to="/signup">Sign Up!</Link>
-      </p>
+      </p> */}
+      <div className={classes["logIn-main"] + " vh-100"}>
+        <div className={classes["h-custom"] + " container-fluid"}>
+          <h1>Logging you in to TripPlanner!</h1>
+          <div className="row d-flex justify-content-center align-items-center h-80">
+            <div className="col-md-9 col-lg-6 col-xl-5">
+              <img
+                src="/unlock.svg"
+                className={classes["signup-img"] + " img-fluid"}
+                alt="Sample image"
+              />
+            </div>
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+              <form onSubmit={handleSubmit}>
+                {/* Email Input */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Enter a valid email address"
+                    name="email"
+                    onChange={handleChange}
+                  />
+                </div>
+
+                {/* Password Input */}
+                <div className="form-outline mb-3">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Enter password"
+                    name="password"
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="text-center text-lg-start mt-4 pt-2">
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-lg"
+                    style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+                  >
+                    Login!
+                  </button>
+                  <p className="small fw-bold mt-2 pt-1 mb-0">
+                    Don't have an account? <Link to="/signup">Sign Up!</Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
